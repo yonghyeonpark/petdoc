4dacff67d50cc7107ca7675d2d44109a24aed3b8
yonghyeonpark <llyyoo93@gmail.com>
2025-03-18 18:27:44 +0900

    feat: 저장, 수정 시간 공통 관리를 위한 클래스 구현

--- src/main/java/yong/petdoc/PetdocApplication.java
+++ src/main/java/yong/petdoc/PetdocApplication.java
@@ -2,12 +2,14 @@ package yong.petdoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class PetdocApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PetdocApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PetdocApplication.class, args);
+    }
 
 }
