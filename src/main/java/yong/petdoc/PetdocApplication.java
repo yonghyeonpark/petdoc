25348486cef68acec350918290134a9f8a17be08
yonghyeonpark <llyyoo93@gmail.com>
2025-03-18 16:59:43 +0900

    initial commit

---
+++ src/main/java/yong/petdoc/PetdocApplication.java
@@ -0,0 +1,13 @@
+package yong.petdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetdocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetdocApplication.class, args);
+	}
+
+}
