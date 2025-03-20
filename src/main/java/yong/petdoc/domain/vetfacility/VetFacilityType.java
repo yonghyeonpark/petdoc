e9ac1cd1d697fe1479dbb2dc5224b905e4de180e
yonghyeonpark <llyyoo93@gmail.com>
2025-03-20 18:52:34 +0900

    feat: 이름을 통한 VetFacilityType 설정 메서드 추가

--- src/main/java/yong/petdoc/domain/vetfacility/VetFacilityType.java
+++ src/main/java/yong/petdoc/domain/vetfacility/VetFacilityType.java
@@ -1,6 +1,20 @@
 package yong.petdoc.domain.vetfacility;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public enum VetFacilityType {
-    HOSPITAL,
-    PHARMACY
+    HOSPITAL("동물병원"),
+    PHARMACY("동물약국");
+
+    private final String name;
+
+    public static VetFacilityType fromName(String name) {
+        for (VetFacilityType vetFacilityType : VetFacilityType.values()) {
+            if (vetFacilityType.name.equals(name)) {
+                return vetFacilityType;
+            }
+        }
+        throw new RuntimeException();
+    }
 }
