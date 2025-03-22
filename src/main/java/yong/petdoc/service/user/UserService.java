15328adfce26b07790dc058a9bc9a8b856f47379
yonghyeonpark <llyyoo93@gmail.com>
2025-03-22 16:59:09 +0900

    fix: 누락된 어노테이션 추가

--- src/main/java/yong/petdoc/service/user/UserService.java
+++ src/main/java/yong/petdoc/service/user/UserService.java
@@ -2,10 +2,12 @@ package yong.petdoc.service.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import yong.petdoc.domain.user.UserRepository;
 import yong.petdoc.web.user.dto.request.CreateUserRequest;
 
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class UserService {
 
--- src/main/java/yong/petdoc/web/user/UserController.java
+++ src/main/java/yong/petdoc/web/user/UserController.java
@@ -2,6 +2,7 @@ package yong.petdoc.web.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class UserController {
 
     private final UserService userService;
 
+    @PostMapping
     public ResponseEntity<Void> createUser(
             @RequestBody CreateUserRequest request,
             UriComponentsBuilder uriComponentsBuilder
