a7e5c276fd8abe164ea5a65ccb509266db02c112
yonghyeonpark <llyyoo93@gmail.com>
2025-03-24 21:25:23 +0900

    refactor: 수의 시설 상세 조회에 즐겨찾기 수 정보 추가

--- src/main/java/yong/petdoc/service/vetfacility/VetFacilityService.java
+++ src/main/java/yong/petdoc/service/vetfacility/VetFacilityService.java
@@ -6,20 +6,23 @@ import org.springframework.transaction.annotation.Transactional;
 import yong.petdoc.domain.vetfacility.VetFacility;
 import yong.petdoc.domain.vetfacility.VetFacilityRepository;
 import yong.petdoc.exception.CustomException;
+import yong.petdoc.service.redis.RedisService;
 import yong.petdoc.web.vetfacility.dto.response.VetFacilityResponse;
 
-import static yong.petdoc.exception.ErrorCode.FACILITY_NOT_FOUND;
+import static yong.petdoc.exception.ErrorCode.VET_FACILITY_NOT_FOUND;
 
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 @Service
 public class VetFacilityService {
 
     private final VetFacilityRepository vetFacilityRepository;
+    private final RedisService redisService;
 
     public VetFacilityResponse getVetFacilityById(Long facilityId) {
+        Long bookmarkCount = redisService.getSizeOfSet(String.valueOf(facilityId));
         VetFacility vetFacility = vetFacilityRepository.findById(facilityId)
-                .orElseThrow(() -> new CustomException(FACILITY_NOT_FOUND));
-        return VetFacilityResponse.from(vetFacility);
+                .orElseThrow(() -> new CustomException(VET_FACILITY_NOT_FOUND));
+        return VetFacilityResponse.from(vetFacility, bookmarkCount);
     }
 }
