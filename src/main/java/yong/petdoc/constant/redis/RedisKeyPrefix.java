bcb03f98626ef3ab13ce03e27601f884ac0d2f98
yonghyeonpark <llyyoo93@gmail.com>
2025-03-25 16:54:21 +0900

    feat: Redis key 관련 상수 모음 클래스 구현

---
+++ src/main/java/yong/petdoc/constant/redis/RedisKeyPrefix.java
@@ -0,0 +1,7 @@
+package yong.petdoc.constant.redis;
+
+public final class RedisKeyPrefix {
+
+    public static final String VET_FACILITY_BOOKMARK = "vetFacility:bookmark:";
+    public static final String VET_FACILITY_DATA = "vetFacility:data:";
+}
