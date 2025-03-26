282b7ca5d03c42611d00216eaf44b3305a361062
yonghyeonpark <llyyoo93@gmail.com>
2025-03-27 02:40:25 +0900

    refactor: Redis key prefix 상수 적용

--- src/main/java/yong/petdoc/service/vetfacility/VetFacilityService.java
+++ src/main/java/yong/petdoc/service/vetfacility/VetFacilityService.java
@@ -9,6 +9,7 @@ import yong.petdoc.exception.CustomException;
 import yong.petdoc.service.redis.RedisService;
 import yong.petdoc.web.vetfacility.dto.response.VetFacilityResponse;
 
+import static yong.petdoc.constant.redis.RedisKeyPrefix.VET_FACILITY_BOOKMARK;
 import static yong.petdoc.exception.ErrorCode.VET_FACILITY_NOT_FOUND;
 
 @RequiredArgsConstructor
@@ -20,9 +21,10 @@ public class VetFacilityService {
     private final RedisService redisService;
 
     public VetFacilityResponse getVetFacilityById(Long facilityId) {
-        Long bookmarkCount = redisService.getSizeOfSet(String.valueOf(facilityId));
         VetFacility vetFacility = vetFacilityRepository.findById(facilityId)
                 .orElseThrow(() -> new CustomException(VET_FACILITY_NOT_FOUND));
+        String key = VET_FACILITY_BOOKMARK + facilityId;
+        Long bookmarkCount = redisService.getSizeOfSet(key);
         return VetFacilityResponse.from(vetFacility, bookmarkCount);
     }
 }
