ef894f673e7ff30b6ddb5ac6b03939967b6b6f67
yonghyeonpark <llyyoo93@gmail.com>
2025-03-27 19:04:01 +0900

    feat: userId, vetFacilityId로 즐겨찾기를 삭제하는 메서드 추가

--- src/main/java/yong/petdoc/domain/bookmark/BookmarkRepository.java
+++ src/main/java/yong/petdoc/domain/bookmark/BookmarkRepository.java
@@ -3,4 +3,6 @@ package yong.petdoc.domain.bookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+
+    public void deleteByUserIdAndVetFacilityId(Long userId, Long vetFacilityId);
 }
