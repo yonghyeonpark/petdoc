4bb2cd9f57c8e8262f5bde7b667193d5b15ab7ca
yonghyeonpark <llyyoo93@gmail.com>
2025-03-27 18:19:00 +0900

    fix: 잘못 설정한 대기 큐 사이즈 수정 및 예외 정책 추가

--- src/main/java/yong/petdoc/config/TaskExecutorConfig.java
+++ src/main/java/yong/petdoc/config/TaskExecutorConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.util.concurrent.ThreadPoolExecutor;
+
 @Configuration
 public class TaskExecutorConfig {
 
@@ -13,7 +15,8 @@ public class TaskExecutorConfig {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(12);
         executor.setMaxPoolSize(50);
-        executor.setQueueCapacity(100);
+        executor.setQueueCapacity(0);
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         executor.setThreadNamePrefix("taskExecutor-");
         executor.initialize();
         return executor;
