537d130a3a7b142d8cc52c7f2c2c8a82c283a113
yonghyeonpark <llyyoo93@gmail.com>
2025-04-20 02:23:16 +0900

    feat: 리뷰 엔티티 클래스 및 리포지토리 구현

---
+++ src/main/java/yong/petdoc/domain/review/Review.java
@@ -0,0 +1,36 @@
+package yong.petdoc.domain.review;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import yong.petdoc.domain.user.User;
+import yong.petdoc.domain.vetfacility.VetFacility;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private VetFacility vetFacility;
+
+    public Review(
+            String comment,
+            User user,
+            VetFacility vetFacility
+    ) {
+        this.comment = comment;
+        this.user = user;
+        this.vetFacility = vetFacility;
+    }
+}
