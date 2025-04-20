42258869362809b17dc6f8904ebb3e7635818230
yonghyeonpark <llyyoo93@gmail.com>
2025-04-20 17:01:15 +0900

    fix: 잘못된 변수명 수정

--- src/main/java/yong/petdoc/web/bookmark/dto/response/MyBookmarksResponse.java
+++ src/main/java/yong/petdoc/web/bookmark/dto/response/MyBookmarksResponse.java
@@ -3,7 +3,7 @@ package yong.petdoc.web.bookmark.dto.response;
 import java.util.List;
 
 public record MyBookmarksResponse(
-        List<BookmarkResponse> reviews,
+        List<BookmarkResponse> bookmarks,
         int page,
         int size,
         int totalPages,
