ed367c252404f61ca9607ccacba6cc13f054eaec
yonghyeonpark <llyyoo93@gmail.com>
2025-04-21 00:28:39 +0900

    feat: 최근 조회 시설 목록 관련 key prefix 추가

--- src/main/java/yong/petdoc/constant/redis/RedisKey.java
+++ src/main/java/yong/petdoc/constant/redis/RedisKey.java
@@ -2,6 +2,10 @@ package yong.petdoc.constant.redis;
 
 public final class RedisKey {
 
+    // 즐겨찾기 관련
     public static final String VET_FACILITY_BOOKMARK_PREFIX = "vet-facility:bookmark:";
     public static final String VET_FACILITY_BOOKMARK_TARGET_IDS = "vet-facility:bookmark:target-ids";
+
+    // 최근 조회 관련
+    public static final String VET_FACILITY_RECENT_BY_USER_PREFIX = "vet-facility:recent:user";
 }
