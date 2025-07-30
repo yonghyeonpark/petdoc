6272db12aeaf9e52fa521512caf0e42608e8d926
yonghyeonpark <llyyoo93@gmail.com>
2025-07-30 16:14:37 +0900

    fix: 잘못된 SQL 문 수정

--- src/main/java/yong/petdoc/config/BatchConfig.java
+++ src/main/java/yong/petdoc/config/BatchConfig.java
@@ -1,6 +1,10 @@
 package yong.petdoc.config;
 
-import lombok.RequiredArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.concurrent.Future;
+
+import javax.sql.DataSource;
+
 import org.apache.poi.ss.usermodel.Row;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.io.WKTWriter;
@@ -22,99 +26,97 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import lombok.RequiredArgsConstructor;
 import yong.petdoc.batch.ExcelProcessor;
 import yong.petdoc.batch.ExcelReader;
 import yong.petdoc.domain.vetfacility.VetFacility;
 import yong.petdoc.service.kakao.KakaoApiService;
 
-import javax.sql.DataSource;
-import java.time.LocalDateTime;
-import java.util.concurrent.Future;
-
 @RequiredArgsConstructor
 @Configuration
 public class BatchConfig {
 
-    private final JobRepository jobRepository;
-    private final PlatformTransactionManager platformTransactionManager;
-    private final KakaoApiService kakaoApiService;
-    private final GeometryFactory geometryFactory;
-    private final TaskExecutor batchTaskExecutor;
-    private final DataSource dataSource;
+	private final JobRepository jobRepository;
+	private final PlatformTransactionManager platformTransactionManager;
+	private final KakaoApiService kakaoApiService;
+	private final GeometryFactory geometryFactory;
+	private final TaskExecutor batchTaskExecutor;
+	private final DataSource dataSource;
 
-    @Bean
-    public Job uploadDataJob() {
-        return new JobBuilder("uploadVetFacilityJob", jobRepository)
-                .start(uploadDataStep())
-                .build();
-    }
+	@Bean
+	public Job uploadDataJob() {
+		return new JobBuilder("uploadVetFacilityJob", jobRepository)
+			.start(uploadDataStep())
+			.build();
+	}
 
-    @Bean
-    @JobScope
-    public Step uploadDataStep() {
-        return new StepBuilder("saveDataStep", jobRepository)
-                .<Row, Future<VetFacility>>chunk(100, platformTransactionManager)
-                .reader(excelReader(null))
-                .processor(asyncProcessor())
-                .writer(asyncWriter())
-                .build();
-    }
+	@Bean
+	@JobScope
+	public Step uploadDataStep() {
+		return new StepBuilder("saveDataStep", jobRepository)
+			.<Row, Future<VetFacility>>chunk(100, platformTransactionManager)
+			.reader(excelReader(null))
+			.processor(asyncProcessor())
+			.writer(asyncWriter())
+			.build();
+	}
 
-    @Bean
-    @StepScope
-    public ItemStreamReader<Row> excelReader(@Value("#{jobParameters['filePath']}") String filePath) {
-        return new ExcelReader(filePath);
-    }
+	@Bean
+	@StepScope
+	public ItemStreamReader<Row> excelReader(@Value("#{jobParameters['filePath']}") String filePath) {
+		return new ExcelReader(filePath);
+	}
 
-    // 비동기 작업 처리를 위한 AsyncItem 활용
-    @Bean
-    public AsyncItemProcessor<Row, VetFacility> asyncProcessor() {
-        AsyncItemProcessor<Row, VetFacility> asyncItemProcessor = new AsyncItemProcessor<>();
-        asyncItemProcessor.setDelegate(processor());
-        asyncItemProcessor.setTaskExecutor(batchTaskExecutor);
-        return asyncItemProcessor;
-    }
+	// 비동기 작업 처리를 위한 AsyncItem 활용
+	@Bean
+	public AsyncItemProcessor<Row, VetFacility> asyncProcessor() {
+		AsyncItemProcessor<Row, VetFacility> asyncItemProcessor = new AsyncItemProcessor<>();
+		asyncItemProcessor.setDelegate(processor());
+		asyncItemProcessor.setTaskExecutor(batchTaskExecutor);
+		return asyncItemProcessor;
+	}
 
-    @Bean
-    public AsyncItemWriter<VetFacility> asyncWriter() {
-        AsyncItemWriter<VetFacility> asyncItemWriter = new AsyncItemWriter<>();
-        asyncItemWriter.setDelegate(writer());
-        return asyncItemWriter;
-    }
+	@Bean
+	public AsyncItemWriter<VetFacility> asyncWriter() {
+		AsyncItemWriter<VetFacility> asyncItemWriter = new AsyncItemWriter<>();
+		asyncItemWriter.setDelegate(writer());
+		return asyncItemWriter;
+	}
 
-    @Bean
-    public ItemProcessor<Row, VetFacility> processor() {
-        return new ExcelProcessor(kakaoApiService, geometryFactory);
-    }
+	@Bean
+	public ItemProcessor<Row, VetFacility> processor() {
+		return new ExcelProcessor(kakaoApiService, geometryFactory);
+	}
 
-    // JDBC의 Batch Update 기능을 활용한 Bulk Insert 처리
-    @Bean
-    public JdbcBatchItemWriter<VetFacility> writer() {
-        String sql = """
-                INSERT INTO vet_facility
-                (vet_facility_type, province, name, location, lot_address, road_address, phone_number, place_url, bookmark_count, is_deleted, created_at, last_modified_at)
-                VALUES
-                (?, ?, ?, ST_GeomFromText(?), ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                """;
-        return new JdbcBatchItemWriterBuilder<VetFacility>()
-                .dataSource(dataSource)
-                .sql(sql)
-                .itemPreparedStatementSetter((item, ps) -> {
-                    ps.setString(1, item.getVetFacilityType().name());
-                    ps.setString(2, item.getProvince().name());
-                    ps.setString(3, item.getName());
-                    ps.setObject(4, new WKTWriter().write(item.getLocation()));
-                    ps.setString(5, item.getLotAddress());
-                    ps.setString(6, item.getRoadAddress());
-                    ps.setString(7, item.getPhoneNumber());
-                    ps.setString(8, item.getPlaceUrl());
-                    ps.setLong(9, item.getBookmarkCount());
-                    ps.setBoolean(10, item.getIsDeleted());
+	// JDBC의 Batch Update 기능을 활용한 Bulk Insert 처리
+	@Bean
+	public JdbcBatchItemWriter<VetFacility> writer() {
+		String sql = """
+			INSERT INTO vet_facility
+			(vet_facility_type, province, name, location, lot_address, road_address, phone_number, place_url, bookmark_count, is_deleted, created_at, last_modified_at)
+			VALUES
+			(?, ?, ?, ST_GeomFromText(?), ?, ?, ?, ?, ?, ?, ?, ?)
+			""";
+		return new JdbcBatchItemWriterBuilder<VetFacility>()
+			.dataSource(dataSource)
+			.sql(sql)
+			.itemPreparedStatementSetter((item, ps) -> {
+				ps.setString(1, item.getVetFacilityType().name());
+				ps.setString(2, item.getProvince().name());
+				ps.setString(3, item.getName());
+				ps.setObject(4, new WKTWriter().write(item.getLocation()));
+				ps.setString(5, item.getLotAddress());
+				ps.setString(6, item.getRoadAddress());
+				ps.setString(7, item.getPhoneNumber());
+				ps.setString(8, item.getPlaceUrl());
+				ps.setLong(9, item.getBookmarkCount());
+				ps.setBoolean(10, item.getIsDeleted());
 
-                    LocalDateTime now = LocalDateTime.now();
-                    ps.setObject(11, now);
-                    ps.setObject(12, now);
-                })
-                .build();
-    }
+				LocalDateTime now = LocalDateTime.now();
+				ps.setObject(11, now);
+				ps.setObject(12, now);
+			})
+			.build();
+	}
 }
