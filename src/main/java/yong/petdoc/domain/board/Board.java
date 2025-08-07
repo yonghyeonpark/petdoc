17e1fd2eff905036b039c38677436cf5a7d5aa93
yonghyeonpark <llyyoo93@gmail.com>
2025-08-08 03:31:20 +0900

    feat: 게시글 엔티티 클래스 및 리포지토리 구현

---
+++ src/main/java/yong/petdoc/domain/board/Board.java
@@ -0,0 +1,47 @@
+package yong.petdoc.domain.board;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import yong.petdoc.domain.BaseTimeEntity;
+import yong.petdoc.domain.user.User;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Board extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String title;
+	private String content;
+	private AtomicInteger views;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private User user;
+
+	public Board(
+		String title,
+		String content,
+		User user
+	) {
+		this.title = title;
+		this.content = content;
+		this.user = user;
+		this.views = new AtomicInteger(0);
+	}
+
+	public void incrementViews() {
+		views.incrementAndGet();
+	}
+}
