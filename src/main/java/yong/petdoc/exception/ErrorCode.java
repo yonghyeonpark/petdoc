3ead96c91e0498a997f62ce9e79f5a2ef2fb7cff
yonghyeonpark <llyyoo93@gmail.com>
2025-08-08 14:59:52 +0900

    feat: BOARD_NOT_FOUND 에러 코드 추가

--- src/main/java/yong/petdoc/exception/ErrorCode.java
+++ src/main/java/yong/petdoc/exception/ErrorCode.java
@@ -1,38 +1,42 @@
 package yong.petdoc.exception;
 
+import org.springframework.http.HttpStatus;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
 
-    // VetFacility 관련
-    VET_FACILITY_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 시설입니다."),
-    INVALID_VET_FACILITY_TYPE(HttpStatus.BAD_REQUEST, "잘못된 동물 시설 타입입니다."),
-    INVALID_PROVINCE(HttpStatus.BAD_REQUEST, "잘못된 지역명입니다."),
+	// VetFacility 관련
+	VET_FACILITY_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 시설입니다."),
+	INVALID_VET_FACILITY_TYPE(HttpStatus.BAD_REQUEST, "잘못된 동물 시설 타입입니다."),
+	INVALID_PROVINCE(HttpStatus.BAD_REQUEST, "잘못된 지역명입니다."),
 
-    // User 관련
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
+	// User 관련
+	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
 
-    // Bookmark 관련
-    BOOKMARK_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 즐겨찾기입니다."),
-    DUPLICATE_BOOKMARK(HttpStatus.CONFLICT, "해당 가게는 이미 즐겨찾기한 상태입니다."),
+	// Bookmark 관련
+	BOOKMARK_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 즐겨찾기입니다."),
+	DUPLICATE_BOOKMARK(HttpStatus.CONFLICT, "해당 가게는 이미 즐겨찾기한 상태입니다."),
 
-    // 파일 관련
-    FILE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다."),
-    FILE_OPEN_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "파일을 여는 중 문제가 발생했습니다."),
-    FILE_CLOSE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "파일을 닫는 중 문제가 발생했습니다."),
-    FILE_NOT_FOUND(HttpStatus.NOT_FOUND, "파일을 찾을 수 없습니다."),
+	// Board 관련
+	BOARD_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 게시글입니다."),
 
-    // 배치 작업 관련
-    JOB_NOT_FOUND(HttpStatus.NOT_FOUND, "요청한 배치 작업을 찾을 수 없습니다."),
-    JOB_EXECUTION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "배치 작업 실행 중 문제가 발생했습니다."),
+	// 파일 관련
+	FILE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다."),
+	FILE_OPEN_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "파일을 여는 중 문제가 발생했습니다."),
+	FILE_CLOSE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "파일을 닫는 중 문제가 발생했습니다."),
+	FILE_NOT_FOUND(HttpStatus.NOT_FOUND, "파일을 찾을 수 없습니다."),
 
-    // JSON 직렬화 관련
-    JSON_SERIALIZATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "JSON 직렬화 중 문제가 발생했습니다.");
+	// 배치 작업 관련
+	JOB_NOT_FOUND(HttpStatus.NOT_FOUND, "요청한 배치 작업을 찾을 수 없습니다."),
+	JOB_EXECUTION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "배치 작업 실행 중 문제가 발생했습니다."),
 
-    private final HttpStatus httpStatus;
-    private final String message;
+	// JSON 직렬화 관련
+	JSON_SERIALIZATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "JSON 직렬화 중 문제가 발생했습니다.");
+
+	private final HttpStatus httpStatus;
+	private final String message;
 }
